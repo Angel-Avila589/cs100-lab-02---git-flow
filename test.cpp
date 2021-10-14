26a6e48e9d1053019eadc1e269672b7c30d0c26a
rfairooz <rfair008@ucr.edu>
2021-10-13 17:30:35 -0700

    Fixes #3

--- test.cpp
+++ test.cpp
@@ -1,36 +1,34 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
-	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
-	EXPECT_EQ("hello world", echo(3, test_val));
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
 }
 
 TEST(EchoTest, EmptyString) {
-	char* test_val[1]; test_val[0] = "./c-echo";
-	EXPECT_EQ("", echo(1, test_val));
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
 
-TEST(EchoTest, SameString) {
-	char* test_val1[2]; test_val1[0] = "./c-echo"; test_val1[1] = "Are we the same?";
-	char* test_val2[2]; test_val2[0] = "./c-echo"; test_val2[1] = "Are we the same?";
-	EXPECT_EQ(echo(2, test_val1), echo(2, test_val2));  
-
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
 
-TEST(EchoTest, CombineStrings) {
-	char* test_val1[2]; test_val1[0] = "./c-echo"; test_val1[1] = "Hello";
-	char* test_val2[2]; test_val2[0] = "./c-echo"; test_val2[1] = "World";
-	EXPECT_EQ("HelloWorld", echo(2, test_val1) + echo(2, test_val2) );
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
 
-TEST(EchoTest, CaseSensitiveString) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "TEST";
-	EXPECT_FALSE("test" ==  echo(2, test_val));
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
